06deb9617f32d08d973857e0d7981b6b5fa57810
Strannik93 <sergevsc@mail.ru>
2023-06-22 10:59:38 +0600

    реализовано левостороннее красно-черное дерево

--- seminar4/Main.java
+++ seminar4/Main.java
@@ -3,9 +3,28 @@ package seminar4;
 public class Main {
     public static void main(String[] args) {
         Tree<Integer> tree = new Tree<>();
+        tree.print();
         tree.add(5);
         tree.add(4);
         tree.add(6);
+        tree.add(6);
+        tree.add(10);
+        tree.add(1);
+        tree.add(20);
+        tree.add(15);
+        tree.add(35);
+        tree.add(3);
+        tree.add(40);
+        tree.add(8);
+        tree.add(50);
+        tree.add(29);
+        tree.add(45);
+        tree.add(33);
+        tree.add(70);
+        tree.add(62);
+        tree.add(67);
+        tree.add(9);
         System.out.println(tree.contains(7));
+        tree.print();
     }
 }
--- seminar4/Tree.java
+++ seminar4/Tree.java
@@ -1,7 +1,8 @@
 package seminar4;
 
-/* 
-** Tree
+/**
+ * левостороннее красно-черное дерево
+ * (left-sided red-black tree)
  */
 public class Tree<V extends Comparable<V>> {
     private Node root;
@@ -21,31 +22,134 @@ public class Tree<V extends Comparable<V>> {
         return false;
     }
 
-    public void add(V value){
-        Node node = root;
-        Node newNode = new Node();
-        newNode.value = value;
-        if (root == null){
-            root = newNode;
+    public boolean add(V value) {
+        if (root != null) {
+            boolean result = addNode(root, value);
+            root = rebalance(root);
+            root.color = Color.BLACK;
+            if (result) System.out.println("Элемент успешно добавлен: " + value);
+            else System.out.println("Элемент уже есть: " + value);
+            return result;
+        } else {
+            root = new Node();
+            root.color = Color.BLACK;
+            root.value = value;
+            System.out.println("Элемент добавлен(было пустое дерево): " + value);
+            return true;
+        } 
+    }
+    
+    private boolean addNode(Node node, V value) {
+        if (node.value.compareTo(value) == 0) {
+            return false;
+        } else {
+            if (node.value.compareTo(value) > 0) {
+                if (node.left != null) {
+                    boolean result = addNode(node.left, value);
+                    node.left = rebalance(node.left);
+                    return result;
+                } else {
+                    node.left = new Node();
+                    node.left.color = Color.RED;
+                    node.left.value = value;
+                    return true;
+                }
+            } else {
+                if (node.right != null) {
+                    boolean result = addNode(node.right, value);
+                    node.right = rebalance(node.right);
+                    return result;
+                } else {
+                    node.right = new Node();
+                    node.right.color = Color.RED;
+                    node.right.value = value;
+                    return true;
+                }
+            }
         }
-        else{
-            if (node.value.compareTo(value)> 0){
-                node.left = newNode;
-            }
-            else if (node.value.compareTo(value)< 0) {
-                node.right = newNode;
-            }
-            else {
-                System.out.println("Такой элемент уже есть");
-            }
-        }
+    }
 
+    private Node rebalance(Node node) {
+        Node result = node;
+        boolean balance;
+        do {
+            balance = false;
+            if (result.right != null && result.right.color == Color.RED &&
+                (result.left == null || result.left.color == Color.BLACK)) {
+                    balance = true;
+                    result = rightSwap(result);
+                }
+            if (result.left != null && result.left.color == Color.RED &&
+                result.left.left != null && result.left.left.color == Color.RED) {
+                    balance = true;
+                    result = leftSwap(result);
+                }
+            if (result.left != null && result.left.color == Color.RED &&
+                result.right != null && result.right.color == Color.RED) {
+                    balance = true;
+                    colorSwap(result);
+                }
+        } while (balance);
+        return result;
+    }
+
+    private Node rightSwap(Node node) {
+        Node right = node.right;
+        Node between = right.left;
+        right.left = node;
+        node.right = between;
+        right.color = node.color;
+        node.color = Color.RED;
+        return right;
+    }
+
+    private Node leftSwap(Node node) {
+        Node left = node.left;
+        Node between = left.right;
+        left.right = node;
+        node.left = between;
+        left.color = node.color;
+        node.color = Color.RED;
+        return left;
+    }
+
+    private void colorSwap(Node node) {
+        node.right.color = Color.BLACK;
+        node.left.color = Color.BLACK;
+        node.color = Color.RED;
+    }
+
+    public void print() {
+        if (root == null) System.out.println("Дерево пустое");
+        else print(root);
+    }
+
+    private void print(Node node) {
+        Node left = node.left;
+        Node right = node.right;
+        System.out.print(node + " -> left: ");
+        if (left == null) System.out.print("null, right: ");
+        else System.out.print(left + ", right: ");
+        if (right == null) System.out.println("null");
+        else System.out.println(right);
+        if (left != null) print(left);
+        if (right != null) print(right);
     }
 
     private class Node{
         private V value;
+        private Color color;
         private Node left;
         private Node right;
+
+        @Override
+        public String toString() {
+            return value + "(" + color + ")";
+        }
+    }
+
+    private enum Color {
+        RED, BLACK
     }
 }
 
